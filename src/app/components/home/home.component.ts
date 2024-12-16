b2ceacdb319fa78477126033da4e1e4b7f4a5b27
Tommaso-Desiato <tommaso_desiato@libero.it>
2024-12-16 12:19:44 +0100

    button fix

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CardModule } from "primeng/card";
 import { ButtonModule } from "primeng/button";
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CardModule, ButtonModule],
+  imports: [CardModule, ButtonModule, RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
